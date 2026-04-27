0687268b58afd0d3ae4e721c49c4f222d6c44166
HeapHeapHooray <heapheaphooray@hotmail.com>
2026-04-26 22:38:04 -0300

    First commit.

---
+++ remote_cookies.sh
@@ -0,0 +1,5 @@
+
+pip install pycookiecheat
+python -m pycookiecheat -o cookies.txt "https://youtube.com"
+sudo cp ./cookies.txt /var/remote_cookies.txt
+scp ./cookies.txt root@nyancat.nu:/var/remote_cookies.txt
